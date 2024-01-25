484c7cf5cf2333b629fff10441681fc10719131d
bendello <bendello123@gmail.com>
2024-01-25 12:47:47 +0000

    Fix :)

--- Home work 3 2nd.py
+++ Home work 3 2nd.py
@@ -3,10 +3,10 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     for numbers in [min, max, quantity]:
-        if (1 <= numbers <= 1000):
+        if (1 <= numbers <= 1000) and min <= max:
             unique_numbers = random.sample(range(min, max + 1), quantity)
             return sorted(unique_numbers)
         else:
             return[]
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(49, 1, 6)
 print("Your lottery numbers are: ", lottery_numbers)
