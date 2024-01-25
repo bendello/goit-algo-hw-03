61355655dc31eac4f80f304d541cd1091e8785c0
bendello <bendello123@gmail.com>
2024-01-25 09:19:32 +0000

    1st commit

---
+++ Home work 3.py
@@ -0,0 +1,15 @@
+#Home work 3
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        user_date = datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.today()
+        difference = current_date - user_date
+        return difference.days
+    except ValueError:
+        return "Incorrect date format. Please use the 'YYYY-MM-DD' format."
+date = input("Input date in format: yyyy-mm-dd ")
+
+print(get_days_from_today(date))
+
